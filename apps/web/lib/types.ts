e256cb131b57c044bcfd739262eede58c1148173
updalla-apshir <updallaapshir@gmail.com>
2025-06-14 17:31:47 +0300

    fomState error fields with array

--- apps/web/lib/types.ts
+++ apps/web/lib/types.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export type FormState =
   | {
       error?: {
-        name?: string;
-        email?: string;
+        name?: string[];
+        email?: string[];
         password?: string[];
       };
       message?: string;
