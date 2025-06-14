02d86232aabaf4db8a12d3a328d0c88a716c7d15
updalla-apshir <updallaapshir@gmail.com>
2025-06-14 11:54:58 +0300

    layout in auth page

--- apps/web/app/auth/Layout.tsx
+++ apps/web/app/auth/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="">Layout</div>;
+  return <div className="">{children}</div>;
 };
 
 export default Layout;
