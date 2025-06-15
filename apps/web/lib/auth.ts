77f590826fe81b5c6ef64d4ac7e3ec8c0ab3058e
updalla-apshir <updallaapshir@gmail.com>
2025-06-15 16:13:20 +0300

    signIn form

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -2,7 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { BACKEND_URL } from "./constants";
-import { LoginFormSchema, SignupFormSchema } from "./type";
+import { SignupFormSchema, FormState } from "./type";
 
 export async function signUp(
   state: FormState,
@@ -36,49 +36,4 @@ export async function signUp(
           ? "The user is already existed!"
           : response.statusText,
     };
-}
-
-export async function signIn(
-  state: FormState,
-  formData: FormData
-): Promise<FormState> {
-  const validatedFields = LoginFormSchema.safeParse({
-    email: formData.get("email"),
-    password: formData.get("password"),
-  });
-
-  if (!validatedFields.success) {
-    return {
-      error: validatedFields.error.flatten().fieldErrors,
-    };
-  }
-
-  const response = await fetch(`${BACKEND_URL}/auth/signin`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(validatedFields.data),
-  });
-
-  if (response.ok) {
-    const result = await response.json();
-    // TODO: Create The Session For Authenticated User.
-
-    await createSession({
-      user: {
-        id: result.id,
-        name: result.name,
-        role: result.role,
-      },
-      accessToken: result.accessToken,
-      refreshToken: result.refreshToken,
-    });
-    redirect("/");
-  } else {
-    return {
-      message:
-        response.status === 401 ? "Invalid Credentials!" : response.statusText,
-    };
-  }
 }
